b76a5eef433f0433b261d8ee81562de52150a333
Matvii Malakhov <matvii.malakhov@student.tuke.sk>
2021-10-13 14:10:32 +0200

    1task

---
+++ ps1/task_1.c
@@ -0,0 +1,82 @@
+#include <superkarel.h>
+void jump_over();
+void turn_right();
+void jump_over2();
+
+int main(){
+turn_on("task_1.kw");
+set_step_delay(100);
+
+  put_beeper();
+  turn_left();
+  step();
+  turn_right();
+    while(no_beepers_present()){
+       if(front_is_clear()){
+        step();
+        }
+        else{
+           jump_over();
+          }
+          }
+     pick_beeper();
+ while(no_beepers_present()){
+       if(front_is_clear()){
+          step();
+          }
+       else{
+     jump_over2();
+     }
+     }
+     pick_beeper();
+     turn_left();
+     turn_left();
+
+            turn_off();
+             return 0;
+               }
+
+
+  void jump_over(){
+  turn_left();
+  while (right_is_blocked()){
+      step();
+      }
+      turn_right();
+      do {
+         step();
+          }
+      while (right_is_blocked());
+              turn_right();
+              while (front_is_clear()){
+                     step();
+                    }
+  turn_left();
+    }
+
+
+
+ void jump_over2(){
+   turn_left();
+   while (left_is_blocked()){
+       step();
+       }
+       turn_left();
+       do {
+          step();
+           }
+       while (left_is_blocked());
+               turn_left();
+               while (front_is_clear()){
+                      step();
+                     }
+  turn_left();
+     }
+
+
+  void turn_right(){
+  turn_left();
+  turn_left();
+  turn_left();
+
+ }
