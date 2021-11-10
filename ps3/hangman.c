da3c9c72da0f8b03907a08b2941b60f6f4a35ec8
Matvii Malakhov <matvii.malakhov@student.tuke.sk>
2021-11-10 23:16:32 +0100

    ps3

--- ps3/hangman.c
+++ ps3/hangman.c
@@ -3,7 +3,7 @@
  #include <stdlib.h>
  #include <string.h>
  #include <sys/stat.h>
- #include <stdbool>
+ #include <stdbool.h>
  #include "hangman.h"
 
 void get_guessed_word(const char secret[], const char letters_guessed[], char guessed_word[]) { 
