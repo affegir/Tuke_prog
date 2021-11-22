c45a6b79d02b6cfb253540fea0322cacd1b24537
Matvii Malakhov <matvii.malakhov@student.tuke.sk>
2021-11-22 21:03:07 +0100

    12task

---
+++ ps5/qr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+ 
+
+
+void encode_char(const char character, bool bits[8])
+{
+    char p = character;
+    int i;
+    for (i = 7; i >= 0; i--)
+    {
+        bits[i] = p & 1;
+        p >>= 1;
+    }
+}
+
+char decode_byte(const bool bits[8])
+{
+    char c = 0;
+    for (int i = 0; i < 8; ++i) 
+        if (bits[i])
+            c |= 1 << i;
+    return c;
+        }
+int main()
+{
+	return 0;
+}
