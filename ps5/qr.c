9bc19ff11b7e3dca0cb1e09c03f2ad0f433487e7
Matvii Malakhov <matvii.malakhov@student.tuke.sk>
2021-11-24 22:57:31 +0100

    12task

--- ps5/qr.c
+++ ps5/qr.c
@@ -24,7 +24,43 @@ char c = 0;
         c += (bits[i]);
     } 
     return c;
+}
+
+void encode_string(const char string[], bool bytes[strlen(string)+1][8])
+{
+    bool o[8];
+    for (int i = 0; i<strlen(string)+1; i++)
+    {
+        char b = string[i];
+        encode_char(b,o);
+        for(int j = 0; j < 8; j++)
+        {
+            bytes[i][j] = o[j];
         }
+        
+    }
+}
+
+void decode_bytes(const int rows, bool bytes[rows][8], char string[rows])
+{
+    char o[8];
+    char c = 0;
+    for (int i = 0; i<rows; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            o[j] = bytes[i][j];
+            
+            for (int l = 0; l < 8; ++l)
+            {
+                c = c * 2;
+                c = c + (o[l]);
+                string[i] = c;
+            }
+        }
+        
+    }
+}
 
 
 int main()
