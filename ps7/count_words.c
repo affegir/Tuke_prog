5dd3e44de45d1884e89226a4bc08fcf9d6d7a727
Matvii Malakhov <matvii.malakhov@student.tuke.sk>
2021-12-07 21:49:22 +0100

    1done

--- ps7/count_words.c
+++ ps7/count_words.c
@@ -1,87 +1,96 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define akTo if
-#define kedy while
-#define spring char
-#define arr int
+#define funk int
+#define vrat return
+#define how if
+#define pause break
 
-arr main(arr argc, spring *argv[]) {
-    FILE *read = fopen(argv[1], "r");
-    FILE *write = fopen(argv[2], "w");
+funk main(funk argc, char* argv[])
+{
+    FILE *banan = fopen(argv[1], "r");
+    float math, algebra = 0;
+    int computer_calk = 0, calkut = 0, i = 0, name = 0, q = 0, w;
+    char frukt[] = "ananas", letter[5000], nation, ckeck_firest, check_second, first, second;
 
-    spring generic[100 * 100], ch, string_name[100 * 100];
-    arr reakt = 1, array = 0, begin = 0, j = 0, end = 0;
-
-    if (read == NULL) {
-        return 1;
-    }
-
-    kedy ((ch = fgetc(read)) != EOF)
+    how (argc < 2)
     {
-        string_name[j] = ch;
-        j++;
+        printf("wrong number\n");
+        vrat 0;
     }
 
-    if (read == NULL)
+    how (banan == NULL)
     {
-        printf("Cannot open file.\n");
-        return 0;
+        printf("Cant open file\n");
+        vrat 0;
     }
 
-    arr amk = 0;
-    kedy (string_name[amk] != '\0') {
-        begin++;
-        if ((string_name[amk + 4] == 'T') && (string_name[amk + 1] == 'T') && (string_name[amk + 3] == 'R') && (string_name[amk + 2] == 'A') && (string_name[amk + 0] == 'S')) {
-            break;
-        }
-        amk++;
+    for (; (nation = fgetc(banan)) != EOF; i++)
+    {
+        letter[i] = nation;
     }
 
-    arr kam = 0;
-    do {
-        end++;
-        akTo ((string_name[kam + 3] == 'P') && (string_name[kam + 1] == 'T') && (string_name[kam + 2] == 'O') && (string_name[kam + 0] == 'S')) {
-            break;
-        }
+    int qwe = 0;
+    do
+    {
+        how (letter[qwe] == EOF)
+            pause;
 
-        kam++;
-    } kedy (string_name[kam] != '\0');
-
-    arr ikal = 0;
-    kedy (ikal < (end - 2) - (begin + 5)) {
-        akTo (string_name[(begin + 5) + ikal] != ' ') {
+        how (letter[qwe] == ' ' && letter[qwe] >= ' ' && letter[qwe] <= ' ')
+        {
         }
         else
         {
-            reakt++;
-        }
-        akTo (reakt % 2 != 0) {
+            int mk = 0;
+            do
+            {
+                ckeck_firest = (letter[qwe] >= 'A' && letter[qwe] <= 'Z') ? (letter[qwe] + 32) : (letter[qwe]);
+                check_second = (letter[qwe + 1] >= 'A' && letter[qwe + 1] <= 'Z') ? (letter[qwe + 1] + 32) : (letter[qwe + 1]);
+                how ((ckeck_firest == 'a' && check_second == 'n') || name == 1)
+                {
+                    how (ckeck_firest != frukt[mk])
+                    {
+                        name = 0;
+                        pause;
+                    }
+                    else
+                    {
+                        name = 1;
+                        calkut++;
+                        qwe++;
+                    }
+                }
+                mk++;
 
+            } while (mk < 6);
         }
-        else
-        {
-            generic[array] = string_name[(begin + 5) + ikal];
-            array++;
-        }
-        ikal++;
+
+        how (calkut == 6)
+            computer_calk++;
+
+        calkut = 0;
+        qwe++;
+    } while (qwe < 5000);
+
+    banan = fopen(argv[1], "w");
+
+    how (computer_calk != 10 && computer_calk <= 10)
+    {
+        char symb;
+        symb = computer_calk + '0';
+        fputc(symb, banan);
+    }
+    else
+    {
+        q = computer_calk / 10;
+        math = computer_calk / 10.0;
+        algebra = (math + 0.0 - q) * 10;
+        w = algebra;
+        first = q + '0';
+        second = w + '0';
+        fputc(first, banan);
+        fputc(second, banan);
     }
 
-    arr b = 0, a = 0;
-    kedy (generic[b] != '\0') {
-        string_name[a] = generic[b + 1];
-        b++;
-        a++;
-    }
-
-    //arr mn = 0;
-    for(arr mn = 0; string_name[mn] != '\0'; mn++) {
-        fputc(string_name[mn], write);
-    } //kedy (string_name[mn] != '\0');
-
-
-    fclose(read);
-
-    fclose(write);
-    return 0;
+    fclose(banan);
+    vrat 0;
 }
